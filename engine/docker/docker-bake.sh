75f20c1263900dbf2866d83dfdfc3a9a67df5ec4
Berne Campbell <berne.campbell@gmail.com>
2023-10-09 16:03:09 +1100

    Bake glibc and musl

--- engine/docker/docker-bake.sh
+++ engine/docker/docker-bake.sh
@@ -14,9 +14,11 @@ cd ..
 echo "== Build Stage3"
 cd bob-stage3 || exit 1
 BASE_TAG=musl-hardened docker buildx bake --load --set gentoo-stage3.tags=kubler-gentoo/stage3-amd64-musl-hardened:latest --set gentoo-stage3.tags=kubler-gentoo/stage3-amd64-musl-hardened:20230423T164653Z
+BASE_TAG=hardened-nomultilib docker buildx bake --load --set gentoo-stage3.tags=kubler-gentoo/stage3-amd64-hardened-nomultilib-openrc:latest --set gentoo-stage3.tags=kubler-gentoo/stage3-amd64-hardened-nomultilib-openrc:20230423T164653Z
 cd ..
 
-echo "== Build bob-musl-core"
+echo "== Build bob-core"
 cd bob-core || exit 1
-BASE_IMAGE=kubler-gentoo/stage3-amd64-musl-hardened docker buildx bake --load --set core.tags=kubler/bob-musl-core:latest --set core.tags=kubler/bob-musl-core:buildx2
+DEF_CHOST=x86_64-gentoo-linux-musl BASE_IMAGE=kubler-gentoo/stage3-amd64-musl-hardened docker buildx bake --load --set core.tags=kubler/bob-musl-core:latest --set core.tags=kubler/bob-musl-core:buildx2
+BASE_IMAGE=kubler-gentoo/stage3-amd64-hardened-nomultilib-openrc docker buildx bake --load --set core.tags=kubler/bob-core:latest --set core.tags=kubler/bob-core:buildx2
 cd ..
